4e466283cf5e224dbbb5c66901df947c008a166f
David Tolman <dajoto1@gmail.com>
2018-08-16 15:20:58 +0100

    Create resize command

---
+++ dist/commands/resizeBrowserToBodyHeight.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var _events = require('events');
+
+module.exports = class ResizeBrowserToBodyHeight extends _events.EventEmitter {
+    command(resizeOffset = this.client.api.globals.deviance.regression.resizeOffset) {
+        const { api } = this.client;
+        api.execute(() => ({
+            width: document.body.scrollWidth, // eslint-disable-line
+            height: document.body.scrollHeight + resizeOffset // eslint-disable-line
+        }), [resizeOffset], result => {
+            const { width, height } = result.value;
+            api.resizeWindow(width, height, () => {
+                this.emit('complete');
+            });
+        });
+
+        return this;
+    }
+};
